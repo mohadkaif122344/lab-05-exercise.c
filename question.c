1b68f886aaff189894b154b52763aa5979d82e66
mohadkaif122344@gmail.com <mohadkaif122344>
2024-12-01 23:20:03 +0530

    lab 05 program

---
+++ question.c
@@ -0,0 +1,22 @@
+001.Program: Write a program to calculate factorial of a 
+             number using recursion. 
+1.	Write a program to add, subtract, multiply and divide two
+    integers using user- defined type function with return type.
+    
+2.	Write a program to calculate sum of 
+    first 20 natural numbers using recursive function.
+3.	Write a program to generate Fibonacci 
+    series using recursive function.
+4.	Write a program to swap two integers using call by value and
+    call by reference methods of passing arguments to a function.
+5.	Write a program to find sum of digits of the number using 
+    Recursive Function.
+6.	Write a program to read an integer number and print the reverse
+    of that number using recursion.
+7.	Write a C program to find maximum and minimum between two numbers 
+    using functions.
+8.	Write a C program to check whether a number is even or odd
+    using functions.
+9.	Write a C program to check whether a number is prime, Armstrong 
+    or perfect number using functions.
+10.	Write a C program to find power of any number using recursion.
